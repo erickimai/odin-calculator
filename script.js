ea53f080c90b50b3c15c25fa7a6b60b1fccfa18f
Erick <erick.imai.rodrigues@gmail.com>
2023-01-04 20:03:52 -0300

    Fixed Error when dividing by 0

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const buttons = document.querySelectorAll('button');
 const disp = document.querySelector('.disp');
 const hist = document.querySelector('.hist');
+const dec = document.querySelector('#dec');
 
 let displayValue = 0;
 let operatorPressed = 0;
 let operationOnHist = 0
 let storedValue;
 let newValue;
-let operator;
+let operator = '';
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
@@ -18,10 +19,15 @@ buttons.forEach((button) => {
 const displayFunction = function displayNumber (id) {
 	switch (id) {
 		case "0":
-		disp.innerHTML += '0';
-		break;
+			if (operatorPressed === 0 && disp.innerText != 'Error') {
+				disp.innerHTML += '0';
+			} else {
+				disp.innerHTML = '0';
+				operatorPressed = 0;
+			}
+			break;
 		case "1":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error')  {
 				disp.innerHTML += '1';
 			} else {
 				disp.innerHTML = '1';
@@ -29,7 +35,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "2":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '2';
 			} else {
 				disp.innerHTML = '2';
@@ -37,7 +43,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "3":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '3';
 			} else {
 				disp.innerHTML = '3';
@@ -45,7 +51,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "4":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '4';
 			} else {
 				disp.innerHTML = '4';
@@ -53,7 +59,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "5":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '5';
 			} else {
 				disp.innerHTML = '5';
@@ -61,7 +67,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "6":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '6';
 			} else {
 				disp.innerHTML = '6';
@@ -69,7 +75,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "7":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '7';
 			} else {
 				disp.innerHTML = '7';
@@ -77,7 +83,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "8":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '8';
 			} else {
 				disp.innerHTML = '8';
@@ -85,7 +91,7 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "9":
-			if (disp.innerText != '0' && operatorPressed === 0) {
+			if (disp.innerText != '0' && operatorPressed === 0 && disp.innerText != 'Error') {
 				disp.innerHTML += '9';
 			} else {
 				disp.innerHTML = '9';
@@ -93,82 +99,86 @@ const displayFunction = function displayNumber (id) {
 			}
 			break;
 		case "add":
-			operatorPressed = 1;
-			operator = '+';
-			newValue = parseFloat(disp.innerText, 10);
-			if(!operationOnHist) {
-				hist.innerText = disp.innerText + ' + ';
-				operationOnHist = 1;
-				storedValue = newValue;
-			} else {
-				// result = operate ('+', storedValue, newValue);
-				// hist.innerHTML += disp.innerText + ' = ';
-				// disp.innerText = `${result}`;
-				// operationOnHist = 0;
-				// storedValue = result;
-				disp.innerHTML = `${operate (operator, storedValue, newValue)}`;
-				hist.innerHTML = `${storedValue} ${operator} ${newValue} =`
-				storedValue = operate (operator, storedValue, newValue);
-				operationOnHist = 0;
+			if (disp.innerText != 'Error') {
+				operatorPressed = 1;
+				operator = '+';
+				newValue = parseFloat(disp.innerText, 10);
+				if(!operationOnHist) {
+					hist.innerText = disp.innerText + ' + ';
+					operationOnHist = 1;
+					storedValue = newValue;
+				} else {
+					result = operate ('+', storedValue, newValue);
+					hist.innerHTML += disp.innerText + ' = ';
+					disp.innerText = `${result}`;
+					operationOnHist = 0;
+					storedValue = result;
+				}
+				dec.removeAttribute('disabled');
 			}
 			break;
 		case "sub":
-			operatorPressed = 1;
-			operator = '-';
-			newValue = parseFloat(disp.innerText, 10);
-			if(!operationOnHist) {
-				hist.innerText = disp.innerText + ' - ';
-				operationOnHist = 1;
-				storedValue = parseFloat(disp.innerText, 10);
-			} else {
-				result = operate ('-', storedValue, newValue);
-				hist.innerHTML += disp.innerText + ' = ';
-				disp.innerText = `${result}`;
-				operationOnHist = 0;
-				storedValue = result;
+			if (disp.innerText != 'Error') {
+				operatorPressed = 1;
+				operator = '-';
+				newValue = parseFloat(disp.innerText, 10);
+				if(!operationOnHist) {
+					hist.innerText = disp.innerText + ' - ';
+					operationOnHist = 1;
+					storedValue = parseFloat(disp.innerText, 10);
+				} else {
+					result = operate ('-', storedValue, newValue);
+					hist.innerHTML += disp.innerText + ' = ';
+					disp.innerText = `${result}`;
+					operationOnHist = 0;
+					storedValue = result;
+				}
+				dec.removeAttribute('disabled');
 			}
 			break;
 		case "mult":
-			operatorPressed = 1;
-			operator = 'x';
-			newValue = parseFloat(disp.innerText, 10);
-			if(!operationOnHist) {
-				hist.innerText = disp.innerText + ' x ';
-				operationOnHist = 1;
-				storedValue = parseFloat(disp.innerText, 10);
-			} else {
-				// result = operate ('x', storedValue, newValue);
-				// hist.innerHTML += disp.innerText + ' = ';
-				// disp.innerText = `${result}`;
-				// operationOnHist = 0;
-				// storedValue = result;
-				disp.innerHTML = `${operate (operator, storedValue, newValue)}`;
-				hist.innerHTML = `${storedValue} ${operator} ${newValue} =`
-				storedValue = operate (operator, storedValue, newValue);
-				operationOnHist = 0;
+			if (disp.innerText != 'Error') {
+				operatorPressed = 1;
+				operator = 'x';
+				newValue = parseFloat(disp.innerText, 10);
+				if(!operationOnHist) {
+					hist.innerText = disp.innerText + ' x ';
+					operationOnHist = 1;
+					storedValue = parseFloat(disp.innerText, 10);
+				} else {
+					result = operate ('x', storedValue, newValue);
+					hist.innerHTML += disp.innerText + ' = ';
+					disp.innerText = `${result}`;
+					operationOnHist = 0;
+					storedValue = result;
+				}
+				dec.removeAttribute('disabled');
 			}
 			break;
 		case "divi":
-			operatorPressed = 1;
-			operator = '&#247;';
-			newValue = parseFloat(disp.innerText, 10);
-			if(!operationOnHist) {
-				hist.innerHTML = disp.innerText + ' &#247; ';
-				operationOnHist = 1;
-				storedValue = parseFloat(disp.innerText, 10);
-			} else {
-				result = operate ('&#247;', storedValue, newValue);
-				hist.innerHTML += disp.innerText + ' = ';
-				disp.innerText = `${result}`;
-				operationOnHist = 0;
-				storedValue = result;
+			if (disp.innerText != 'Error') {
+				operatorPressed = 1;
+				operator = '&#247;';
+				newValue = parseFloat(disp.innerText, 10);
+				if(!operationOnHist) {
+					hist.innerHTML = disp.innerText + ' &#247; ';
+					operationOnHist = 1;
+					storedValue = parseFloat(disp.innerText, 10);
+				} else {
+					result = operate ('&#247;', storedValue, newValue);
+					hist.innerHTML += disp.innerText + ' = ';
+					disp.innerText = `${result}`;
+					operationOnHist = 0;
+					storedValue = result;
+				}
+				dec.removeAttribute('disabled');
 			}
 			break;
 		case "del":
-			if (disp.innerText != '0') {
+			if (disp.innerText != '0' && disp.innerText != 'Error') {
 				disp.innerText = `${disp.innerText.slice(0, -1)}`;
-			} 
-			if (disp.innerText <= '9') {
+			}
+			if (disp.innerText === '' || disp.innerText === 'Error') {
 				disp.innerText = '0';
 			}
 			break;
@@ -179,19 +189,25 @@ const displayFunction = function displayNumber (id) {
 			displayValue = 0;
 			storedValue = 0;
 			newValue = 0;
+			dec.removeAttribute('disabled');
 			break;
 		case "perc":
 			disp.innerHTML += '%';
 			break;	
 		case "dec":
 			disp.innerHTML += '.';
+			dec.setAttribute('disabled', 'true');
 			break;
 		case "equal":
+			if (operator != '') {
 			newValue = parseFloat(disp.innerText, 10);
 			disp.innerHTML = `${operate (operator, storedValue, newValue)}`;
 			hist.innerHTML = `${storedValue} ${operator} ${newValue} =`
 			storedValue = operate (operator, storedValue, newValue);
 			operationOnHist = 0;
+			operator = '';
+			dec.removeAttribute('disabled');
+			}
 			break;
 	}
   }
@@ -209,6 +225,9 @@ const multiply = function multiplyTwoNumbers (a, b) {
 }
 
 const divide = function divideTwoNumbers (a, b) {
+	if (b === 0) {
+		return ('Error');
+	}
 	return a / b;
 }
 
